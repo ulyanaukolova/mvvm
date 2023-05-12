3f5f0f6dc725754dd1cc200b38711f2bfe56cb04
ulyanaukolova <isip_u.a.ukolova@mpt.ru>
2023-05-12 23:55:35 +0300

    ma

---
+++ MVVM/Model/Polz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Model
+{
+    public class Polz
+    {
+        public Polz(string name, string ip)
+        {
+            Name = name;
+            IP = ip;
+        }
+
+        public string Name { get; set; }
+        public string IP { get; set; }
+    }
+}
---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using MVVM.Model;
+using MVVM.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.ViewModel
+{
+    internal class MainViewModel: BindingHelper
+    {
+        private Polz dan;
+
+        public Polz Dan
+        {
+            get { return dan; }
+            set
+            {
+                dan = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Polz> polzs;
+
+        public ObservableCollection<Polz> Polzs
+        {
+            get { return polzs; }
+            set { polzs = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            Polzs = new ObservableCollection<Polz>()
+            {
+                new Polz("KS", "127.0.0.0"),
+                new Polz("Ulyasha", "26.85.79.42")
+            };
+        }
+    }
+}
